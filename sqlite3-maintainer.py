5a53e7c96c766dfa39b342add7486917f78630cc
sweting <50222844+ThisIsanAlt@users.noreply.github.com>
2021-07-04 17:21:59 -0400

    update

--- sqlite3-maintainer.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('database.db')
-c = conn.cursor()
-
-#c.execute('CREATE TABLE INFO(APIKey, CriticalWins, WarningWins, CriticalNWLVL, WarningNWLVL)')
-c.execute('UPDATE INFO SET APIKey = "5784c506-52f8-44c3-a724-af25cdb61a3f"')
-print(c.execute('SELECT * FROM INFO').fetchall())
-conn.commit()
